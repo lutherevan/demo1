7fe121f0bb5f7b5002c04263f0bc5cd60fb31654
lutherevan <89381055+lutherevan@users.noreply.github.com>
2025-12-16 18:45:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-cimport streamlit as st import option_menu
+import streamlit as st 
+from streamlit_option_menu import option_menu
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
