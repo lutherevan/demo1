559f3cd95a9cd0b2c44d7e96d325b6f229740f22
lutherevan <89381055+lutherevan@users.noreply.github.com>
2025-12-16 18:22:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,6 @@ selected_quarter = st.selectbox(
   "Choose Quarter:",
   ["Q1", "Q2", "Q3", "Q4"])
 
-quarter_revenue.values()
+st.write(quarter_revenue.values())
 
 # st.write(f"Revenue for {selected_quarter}: {quarter_revenue.values[selected_quarter]}")
