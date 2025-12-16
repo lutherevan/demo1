af9434e8de82a3172aabcfe5953a4d635491f3c1
lutherevan <89381055+lutherevan@users.noreply.github.com>
2025-12-16 16:06:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,6 @@ col1, col2, col3 = st.columns(3)
 with col1:
         st.write(f"Q1: {quarterly_revenue["Q1"]}")
 with col2:
-        st.write(f"Q2: {quarterly_revenue["Q2"]}"
+        st.write(f"Q2: {quarterly_revenue["Q2"]}")
 with col3:
         st.write(f"Q3: {quarterly_revenue["Q3"]}")
